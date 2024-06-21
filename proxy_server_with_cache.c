16f414b58bbf19b4ff8ebc65f45c019852abc5c2
RohanNayan <rohannayan405@gmail.com>
2024-06-21 22:00:53 +0530

    Declared Functions

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
+
+#define MAX_CLIENTS 10
 
 typedef struct cache_element cache_element;
 
@@ -12,3 +15,11 @@ struct cache_element{
     time_t lru_time_track;
     cache_element* next;
 };
+
+cache_element* find(char* url);
+int add_cache_element(char* data, int size, char* url);
+void remove_cache_element();
+
+int port_number = 8080;
+int proxy_socketId;
+pthread_t tid[MAX_CLIENTS]; 
